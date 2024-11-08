0a8c62b894c50ff25ca5cbf9bad6d33faf43bf7a
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-08 11:34:39 +0200

    chore: created and connected to db via postgres

--- petstagram_app/settings.py
+++ petstagram_app/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'petstagram_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram_app_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
