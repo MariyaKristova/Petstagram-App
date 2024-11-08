e669a674c6bd0197d31c9a5c8fd8f99318a6b68f
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-08 12:42:09 +0200

    feat: create forms

---
+++ petstagram_app/pets/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+
+from petstagram_app.pets.models import Pet
+
+
+class PetBaseForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ['name', 'date_of_birth', 'personal_photo']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Pet /name'}),
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'personal_photo': forms.TextInput(attrs={'placeholder': 'Link to image'}),
+        }
+
+        labels = {
+            'name': 'Pet name',
+            'date_of_birth': 'Date of Birth',
+            'personal_photo': 'Link to image',
+        }
+
+
+class PetAddForm(PetBaseForm):
+    pass
+
+class PetEditForm(PetBaseForm):
+    pass
+
+class PetDeleteForm(PetBaseForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['disabled'] = 'disabled'
+            field.widget.attrs['readonly'] = 'readonly'
+
