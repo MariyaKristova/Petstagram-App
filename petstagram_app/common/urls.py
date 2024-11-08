f35d7395f4d08947c6a4495fd99b3420a8bff64b
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-08 11:50:44 +0200

    feat: added urls

---
+++ petstagram_app/common/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from petstagram.common import views
+from petstagram.common.views import HomePageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('like/<int:photo_id>/', views.like_functionality, name='like'),
+    path('share/<int:photo_id>/', views.share_functionality, name='share'),
+    path('comment/<int:photo_id>/', views.comment_functionality, name='comment')
+]
