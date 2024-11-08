e2e13798a9080fdc4e9dda54d9dc4d76bdf813a7
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-08 13:06:14 +0200

    feat: updated urls

--- petstagram_app/urls.py
+++ petstagram_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-#     path('', include('petstagram_app.common.urls')),
-#     path('accounts/', include('petstagram_app.accounts.urls')),
-#     path('pets/', include('petstagram_app.pets.urls')),
-#     path('photos/', include('petstagram_app.photos.urls')),
+    path('', include('petstagram_app.common.urls')),
+    path('accounts/', include('petstagram_app.accounts.urls')),
+    path('pets/', include('petstagram_app.pets.urls')),
+    path('photos/', include('petstagram_app.photos.urls')),
 ]
