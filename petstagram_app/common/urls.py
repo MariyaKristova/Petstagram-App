bff18d719b98e19c2fbd17ce5a0c19069522ebdc
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-08 12:33:41 +0200

    feat: updated urls in common app

--- petstagram_app/common/urls.py
+++ petstagram_app/common/urls.py
@@ -1,10 +1,10 @@
-# from django.urls import path
-# from petstagram_app.common import views
-# from petstagram_app.common.views import HomePageView
-#
-# urlpatterns = [
-#     path('', HomePageView.as_view(), name='home'),
-#     path('like/<int:photo_id>/', views.like_functionality, name='like'),
-#     path('share/<int:photo_id>/', views.share_functionality, name='share'),
-#     path('comment/<int:photo_id>/', views.comment_functionality, name='comment')
-# ]
+from django.urls import path
+from petstagram_app.common import views
+from petstagram_app.common.views import HomePageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('like/<int:photo_id>/', views.like_functionality, name='like'),
+    path('share/<int:photo_id>/', views.share_functionality, name='share'),
+    path('comment/<int:photo_id>/', views.comment_functionality, name='comment')
+]
