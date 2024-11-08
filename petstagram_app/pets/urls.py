af1bffe8e6a973d6558da98305b1fa18cd147e54
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-08 12:02:34 +0200

    chore: made changes to urls

--- petstagram_app/pets/urls.py
+++ petstagram_app/pets/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
-from petstagram_app.pets.views import EditPetView, DeletePetView, AddPetView, PetDetailsView
-
-urlpatterns = [
-    path('add/', AddPetView.as_view(), name='add-pet'),
-    path('<str:username>/pet/<slug:pet_slug>/', include([
-    path('', PetDetailsView.as_view(), name='pet-details'),
-    path('edit/', EditPetView.as_view(), name='pet-edit'),
-    path('delete/', DeletePetView.as_view(), name='pet-delete'),
-    ]))
-]
+# from django.urls import path, include
+# from petstagram_app.pets.views import EditPetView, DeletePetView, AddPetView, PetDetailsView
+#
+# urlpatterns = [
+#     path('add/', AddPetView.as_view(), name='add-pet'),
+#     path('<str:username>/pet/<slug:pet_slug>/', include([
+#     path('', PetDetailsView.as_view(), name='pet-details'),
+#     path('edit/', EditPetView.as_view(), name='pet-edit'),
+#     path('delete/', DeletePetView.as_view(), name='pet-delete'),
+#     ]))
+# ]
