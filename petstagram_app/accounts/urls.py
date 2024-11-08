6b4bdff185f0990eb4fea4bdcfc02daab531e0f7
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-08 12:36:50 +0200

    feat: updated urls

--- petstagram_app/accounts/urls.py
+++ petstagram_app/accounts/urls.py
@@ -1,12 +1,12 @@
-# from django.urls import path, include
-# from petstagram_app.accounts import views
-#
-# urlpatterns = [
-#     path('register/', views.register, name='register'),
-#     path('login/', views.login, name='login'),
-#     path('profile/<int:pk>', include([
-#         path('', views.show_profile_details, name='profile-details'),
-#         path('edit/', views.profile_edit, name='profile-edit'),
-#         path('delete', views.delete_profile, name='profile-delete'),
-#     ]))
-# ]
+from django.urls import path, include
+from petstagram_app.accounts import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('profile/<int:pk>', include([
+        path('', views.show_profile_details, name='profile-details'),
+        path('edit/', views.profile_edit, name='profile-edit'),
+        path('delete', views.delete_profile, name='profile-delete'),
+    ]))
+]
