8bc099a6b5fc56fc50da234e5ca343def4741677
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-11 14:55:42 +0200

    update placeholder

--- petstagram_app/common/templatetags/placeholder_filter.py
+++ petstagram_app/common/templatetags/placeholder_filter.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 @register.filter
-def placeholder(value, token):
-    value.field.widget.attrs['placeholder'] = token
+def placeholder(value, text_to_be_placed):
+    value.field.widget.attrs['placeholder'] = text_to_be_placed
     return value
