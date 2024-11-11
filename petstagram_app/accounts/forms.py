8168d8462cf95c016d9300b7396861c73fdf9115
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-11 15:00:44 +0200

    create login form

--- petstagram_app/accounts/forms.py
+++ petstagram_app/accounts/forms.py
@@ -1,5 +1,6 @@
+from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 
 UserModel = get_user_model()
 
@@ -12,3 +13,11 @@ class AppUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = UserModel
         fields = ('email',)
+
+class AppUserLoginForm(AuthenticationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'autofocus': 'True'}))
+    password = forms.CharField(
+        label = "Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
+    )
