f8a26bcd4994cc71a48cbd5c79f82c73dcc62c73
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-13 13:02:08 +0200

    refactor pet posts

--- petstagram_app/accounts/models/app_profile.py
+++ petstagram_app/accounts/models/app_profile.py
@@ -8,3 +8,11 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=30, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     profile_picture = models.URLField(blank=True, null=True)
+
+    def get_profile_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        elif self.first_name or self.last_name:
+            return f"{self.first_name}" or f"{self.last_name}"
+        else:
+            return "Anonymous User"
